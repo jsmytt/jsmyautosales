cd893de4a8222d35988f966ac277f2d7b4ae6594
Tuff Witarama <tuffjunkmail@gmail.com>
2019-01-05 01:30:47 -0800

    Commit before AWS push 01052019

--- JSMY/views.py
+++ JSMY/views.py
@@ -22,10 +22,6 @@ def dictfetchall(cursor):
             for row in cursor.fetchall()
     ]
 
-
-
-
-
 class banner(TemplateView):
     template_name = 'index.html'
     def get(self, request,):
@@ -35,6 +31,17 @@ class banner(TemplateView):
         lenused=len(Car.object.filter(type="Used", publish=True, created__lte=timezone.now()).order_by('-pk'))
         lease = Car.object.filter(type="Lease", publish=True, created__lte=timezone.now()).order_by('-pk')
         lenlease=len(Car.object.filter(type="Lease", publish=True, created__lte=timezone.now()).order_by('-pk'))
+        BImage1 = bn.objects.values()[0]['BImage1']
+        BImage2 = bn.objects.values()[0]['BImage2']
+        BImage3 = bn.objects.values()[0]['BImage3']
+        BImage4 = bn.objects.values()[0]['BImage4']
+        BImage5 = bn.objects.values()[0]['BImage5']
+        Blink1 = bn.objects.values()[0]['Blink1']
+        Blink2 = bn.objects.values()[0]['Blink2']
+        Blink3 = bn.objects.values()[0]['Blink3']
+        Blink4 = bn.objects.values()[0]['Blink4']
+        Blink5 = bn.objects.values()[0]['Blink5']
+
         cursor = connection.cursor()
         cursor.execute(
             'select listing_carimg.LImage, listing_car.price,listing_car.slug, listing_car.title from listing_car inner join listing_carimg on listing_car.id = listing_carimg.car_id where listing_carimg.mainimage = 1 and listing_car.type ="Used" and listing_car.publish = 1 ORDER BY listing_car.created desc ')
@@ -45,9 +52,9 @@ class banner(TemplateView):
 
         dfLease = dictfetchall(cursor)
 
-        context = {"new":new,"lease":lease,"lennew":lennew,"lenused":lenused,"lenlease":lenlease, 'dfUsed':dfUsed,'dfLease':dfLease
-
-
+        context = {"new":new,"lease":lease,"lennew":lennew,"lenused":lenused,"lenlease":lenlease, 'dfUsed':dfUsed,'dfLease':dfLease,
+                   "BImage1": BImage1, "BImage2": BImage2, "BImage3": BImage3, "BImage4": BImage4, "BImage5": BImage5,
+                   "Blink1": Blink1, "Blink2": Blink2, "Blink3": Blink3, "Blink4": Blink4, "Blink5": Blink5
                    }
         return render(request, self.template_name, context)
 
